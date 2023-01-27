eb3c68477e85e1346579a24d481198ae2a51173b
Madan Mohan Singh <99973144+ARNO-0@users.noreply.github.com>
2023-01-27 19:25:37 +0530

    removed varcel logo

--- pages/index.js
+++ pages/index.js
@@ -25,23 +25,7 @@ export default function Home() {
                         Get started by &nbsp;
                         <code className={styles.code}>Minting NFT</code>
                     </p>
-                    <div>
-                        <a
-                            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            By{" "}
-                            <Image
-                                src="/vercel.svg"
-                                alt="Vercel Logo"
-                                className={styles.vercelLogo}
-                                width={100}
-                                height={24}
-                                priority
-                            />
-                        </a>
-                    </div>
+                    
                 </div>
 
                 <div className={styles.center}>
